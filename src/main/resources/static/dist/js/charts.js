4a59878a016239e6032bd8cf008c5a27ff9610ce
Hendrik <drikdoank@gmail.com>
2023-01-31 09:14:47 +0700

    add chart

--- src/main/resources/static/dist/js/charts.js
+++ src/main/resources/static/dist/js/charts.js
@@ -1,42 +1,85 @@
 $(function () {
     var data = {
-        labels : ["January","February","March","April","May","June","July"],
-        datasets : [
-            {
-                fillColor : "rgba(220,220,220,0.5)",
-                strokeColor : "rgba(220,220,220,1)",
-                pointColor : "rgba(220,220,220,1)",
-                pointStrokeColor : "#fff",
-                data : [65,59,90,81,56,55,40]
-            },
-            {
-                fillColor : "rgba(151,187,205,0.5)",
-                strokeColor : "rgba(151,187,205,1)",
-                pointColor : "rgba(151,187,205,1)",
-                pointStrokeColor : "#fff",
-                data : [28,48,40,19,96,27,100]
-            }
-        ]
-    }
-    var options = {};
-    var ctx = document.getElementById("linechartContainer").getContext('2d');
-    new Chart(ctx).Line(data,options);
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    };
 
-    var data = [
-        {
-            value : 30,
-            color: "#D97041"
-        },
-        {
-            value : 90,
-            color: "#C7604C"
-        },
-        {
-            value : 24,
-            color: "#21323D"
+    var options = {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
         }
-    ]
+    };
 
+    var ctx = document.getElementById("barchartContainer").getContext('2d');
+    var myBarChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: options
+    });
+
+    var ctx = document.getElementById("linechartContainer").getContext('2d');
+    var myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: options
+    });
+
+    data = {
+        datasets: [{
+            data: [10, 20, 30],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ]    
+        }],
+        labels: [
+            'Red',
+            'Yellow',
+            'Blue'
+        ]
+    };
+    options = {};
     var ctx = document.getElementById("piechartContainer").getContext('2d');
-    new Chart(ctx).Pie(data,options);
+    var myPieChart = new Chart(ctx,{
+        type: 'pie',
+        data: data,
+        options: options
+    });
+
+    var ctx = document.getElementById("dognutchartContainer").getContext('2d');
+    var myDognutChart = new Chart(ctx,{
+        type: 'doughnut',
+        data: data,
+        options: options
+    });
 });
